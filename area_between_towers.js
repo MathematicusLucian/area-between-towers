c10c2e3ce1d3865786b8d2b79e84c4f7a0221abe
k1n4kut4 <recruitlukejones@gmail.com>
2024-01-21 03:03:49 +0000

    tidied the comments up

--- area_between_towers.js
+++ area_between_towers.js
@@ -1,26 +1,34 @@
-/** CONSTRAINTS
-2 <= n <= 105
-n == height.length
-0 <= height[i] <= 104 */
+/**
+ * CONSTRAINTS
+ * 2 <= n <= 105
+ * n == height.length
+ * 0 <= height[i] <= 104
+ */
 function maxArea(height) {
     var maximumPossibleArea = 0;
     // Loop -> Each Tower (Left to Right)
-    /**  One could use ``for (let i = 0; i < height.length - 1; i++) {``
+    /**
+     * One could use ``for (let i = 0; i < height.length - 1; i++) {``
      * but ``forEach`` is cleaner
-    Could also use ``arr.forEach(function(item, i)`` */
+     * Could also use ``arr.forEach(function(item, i)``
+     */
     height.forEach(function (iValue, iXAxis) {
-        /** This current index is Building I
-
-        We will compare it to the other buildings, with a second loop, where the index is Building J
-
-        There is no pointing comparing to:-
-        Itself, i.e. where Building I and Building J are the same index/value in heights array
-        Avoid duplication of effort; a more efficient algorithm:
-        As the index of Building I increases, there is no point comparing to those before it,
-        because these were considered when they were Building I and the current location was then Building J
-        :. For each tower, consider area ONLY between it and the towers AHEAD
-
-        Determine the towers ahead of Building I */
+        /**
+         * This current index is Building I
+         *
+         * We will compare it to the other buildings, with a second loop, where the index is Building J
+         *
+         * There is no pointing comparing to:-
+         * Itself, i.e. where Building I and Building J are the same index/value in heights array
+         * Avoid duplication of effort; a more efficient algorithm:
+         * As the index of Building I increases, there is no point comparing to those before it,
+         * because these were considered when they were Building I and the current location was then Building J
+         * :. For each tower, consider area ONLY between it and the towers AHEAD
+         *
+         * Determine the towers ahead of Building I
+         * Clone to avoid mutating the oroginal area
+         * Then add 1 because we do not want to include the current Building I
+         */
         var cloneBuildingHeights = height.slice(0);
         var buildingsAhead = cloneBuildingHeights.splice(iXAxis + 1);
         // Loop -> Each Tower (Left to Right)
@@ -28,20 +36,23 @@ function maxArea(height) {
             // Determine the lower of the two towers
             var lowestTower = Math.min(iValue, jValue);
             /** We need the X-axis value, if we are to calculate the area
-            We are comparing two buildings, at Building I and Building J
-            The second array was spliced with the index of Building I
-            :. The second array has the 'height' array values that occur after Building I
-            :. Building J's index in the second array is its distance from Building J
-            Addition of 1 to the value, because computers count from 0
-            
-            Determine area. Multiply:
-                Y-axis as lower tower
-            With:
-                Distance (X-axis): */
-            var current = (lowestTower * (jXAxis + 1));
-            /** If the result is greater than that between previous tower pairs,
-             * updated store maximumPossibleArea value */
-            maximumPossibleArea = Math.max(maximumPossibleArea, current);
+             * We are comparing two buildings, at Building I and Building J
+             * The second array was spliced with the index of Building I
+             * :. The second array has the 'height' array values that occur after Building I
+             * :. Building J's index in the second array is its distance from Building J
+             * Addition of 1 to the value, because computers count from 0
+             *
+             * Determine area. Multiply:
+             *      Y-axis as lower tower
+             * With:
+             *   Distance (X-axis):
+             */
+            var areaAtCurrentIndices = (lowestTower * (jXAxis + 1));
+            /**
+             * If the result is greater than that between previous tower pairs,
+             * updated store maximumPossibleArea value
+             * */
+            maximumPossibleArea = Math.max(maximumPossibleArea, areaAtCurrentIndices);
         });
     });
     return maximumPossibleArea;
