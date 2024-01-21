7355fc087dfa19affcbe8387d1b80d8134f68ab9
k1n4kut4 <recruitlukejones@gmail.com>
2024-01-21 01:40:05 +0000

    area calculation ts, and js files

---
+++ area_between_towers.js
@@ -0,0 +1,46 @@
+// tsc area_between_towers.ts && node area_between_towers.js
+// CONSTRAINTS
+// 2 <= n <= 105
+// n == height.length
+// 0 <= height[i] <= 104
+// const http = require('http'); 
+function maxArea(height) {
+    var maximum = 0;
+    // Loop -> Each Tower (Left to Right)
+    // for (let i = 0; i < height.length - 1; i++) {
+    // to avoid casting i to integer, another option is arr.forEach(function(item, i)
+    height.forEach(function (iValue, i) {
+        // For each tower, consider area between it and the ones ahead
+        // Determine the lower of the two towers and multiply with X-axis
+        // Therefore, store the distance (X-axis)
+        // If area between these two buildings is greater than previously stored area, set as stored area
+        // Loop -> Each Tower (Left to Right)
+        // for (let j = i + 1; j < height.length; j++) {
+        // The distance between the tower in question and any tower prior was already considered, 
+        // when measuring against it from tower prior in the away
+        var cloneBuildingHeights = height.slice(0);
+        var buildingsAhead = cloneBuildingHeights.splice(i + 1);
+        buildingsAhead.forEach(function (jValue, j) {
+            // Determine the lower of the two towers
+            var lowestTower = Math.min(iValue, jValue);
+            // Determine area
+            // Y-axis as lower tower
+            // Distance (X-axis):
+            // We are comparing two buildings, at Building I and Building J
+            // The second array was spliced with the index of Building I
+            // :. The second array has the 'height' array values that occur after Building I
+            // :. Building J's index in the second array is its distance from Building J
+            // Addition of 1 to the value, because computers count from 0
+            var current = (lowestTower * (j + 1));
+            // If area between these two buildings is greater than previously stored area, set as stored area
+            maximum = Math.max(maximum, current);
+        });
+    });
+    return maximum;
+}
+var height = [1, 8, 6, 2, 5, 4, 8, 3, 7];
+var n = height.length;
+console.log(maxArea(height));
+// const server = http.createServer((req, res) => { res.end(); }); 
+// server.listen(3000, () => {
+// });
---
+++ area_with_recursion.ts
@@ -0,0 +1,72 @@
+// tsc area_between_towers.ts && node area_between_towers.js
+
+/** CONSTRAINTS
+2 <= n <= 105
+n == height.length
+0 <= height[i] <= 104 */
+
+function maxArea(height: number[]): number {
+    // ^ Shame this does not allow for recursion
+
+    let maximumPossibleArea = 0;
+ 
+    // Loop -> Each Tower (Left to Right)
+    /**  One could use ``for (let i = 0; i < height.length - 1; i++) {`` 
+     * but ``forEach`` is cleaner
+    Could also use ``arr.forEach(function(item, i)`` */
+    height.forEach((iHeight, iXAxis) => {
+
+        /** This current index is Building I
+
+        We will compare it to the other buildings, with a second loop, where the index is Building J
+
+        There is no pointing comparing to:-
+        Itself, i.e. where Building I and Building J are the same index/value in heights array
+        Avoid duplication of effort; a more efficient algorithm:
+        As the index of Building I increases, there is no point comparing to those before it,
+        because these were considered when they were Building I and the current location was then Building J
+        :. For each tower, consider area ONLY between it and the towers AHEAD
+
+        Determine the towers ahead of Building I */
+        let cloneBuildingHeights = height.slice(0);
+        let buildingsAhead = cloneBuildingHeights.splice(iXAxis + 1);
+
+        compareTowerPairs(iHeight, 0, buildingsAhead, maximumPossibleArea)
+
+    });
+
+    return maximumPossibleArea;
+}
+
+function compareTowerPairs(iHeight: number, jXAxis: number, buildingsAhead: number[], maximumPossibleArea: number) {
+
+        // First Building in Array
+        const buildingJHeight = buildingsAhead[0]
+
+        // Determine the lower of the two towers
+        let lowestTower = Math.min(iHeight,buildingJHeight);
+
+        /** We need the X-axis value, if we are to calculate the area
+        We are comparing two buildings, at Building I and Building J
+        The second array was spliced with the index of Building I
+        :. The second array has the 'height' array values that occur after Building I
+        :. Building J's index in the second array is its distance from Building J
+        Addition of 1 to the value, because computers count from 0 */
+        let current = (lowestTower * (jXAxis + 1));
+
+        /** Determine area. Multiply:
+            Y-axis as lower tower
+        With:
+            Distance (X-axis):
+        If the result is greater than that between previous tower pairs, updated store maximumPossibleArea value */ 
+        maximumPossibleArea = Math.max(maximumPossibleArea, current);
+
+        // Pop the leftmost tower, then proceed to those on the right
+        if(buildingsAhead.length >= 2) {
+            maximumPossibleArea : 
+
+}
+     
+let height = [1,8,6,2,5,4,8,3,7];
+let n = height.length;
+console.log(maxArea(height)); 
